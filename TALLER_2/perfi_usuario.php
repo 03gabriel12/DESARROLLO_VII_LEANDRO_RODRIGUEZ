1cf25dfb7fb4e73f88cb4485425432ee172affd5
03gabriel12 <leandro.rodriguez@utp.ac.pa>
2024-08-20 19:52:56 -0500

    actualizacion

--- TALLER_2/perfi_usuario.php
+++ TALLER_2/perfi_usuario.php
@@ -8,18 +8,30 @@ define("OCUPACION", "Desarrollador de software");
 
 
 $mensaje = OCUPACION . " " . $nombre_completo;
-
+print ("Hola ");
+echo "<br>";
 printf("En resumen: %s, %d años, %s, %s<br>", $nombre_completo, $edad, $correo, $telefono);
 
 echo "<br>Información de debugging:<br>";
-var_dump("Nombre Completo : " . $nombre_completo);
+var_dump("Nombre Completo : " . $nombre_completo . "tengo " . $edad . " años , mi numero de telefono es : " . $telefono . " Mi ocupacion es " . OCUPACION);
 echo "<br>";
-var_dump("edad: " . $edad . " años");
-echo "<br>";
-var_dump("numero de telefono :  " . $telefono);
-echo "<br>";
-var_dump("contante");
 
+echo "-----------------------";
+
+echo "<br>";
+var_dump("Tipos de las variables");
+echo "<br>";
+echo " variable nombre completo ";
+var_dump($nombre_completo);
+echo "<br>";
+echo " variable edad ";
+var_dump($edad);
+echo "<br>";
+echo " variable correo ";
+var_dump($correo);
+echo "<br>";
+echo " variable telefono ";
+var_dump($telefono);
+echo "<br>";
+echo " constante ";
 var_dump(OCUPACION);
-
-echo "<br>";
